36cd4e8ede799ec97b974a17123b1f67b81de8d1
MrBird <852252810@qq.com>
2020-03-26 18:12:27 +0800

    异常处理

--- src/main/java/cc/mrbird/febs/common/handler/GlobalExceptionHandler.java
+++ src/main/java/cc/mrbird/febs/common/handler/GlobalExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -46,7 +47,7 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 统一处理请求参数校验(实体对象传参)
+     * 统一处理请求参数校验(实体对象传参-form)
      *
      * @param e BindException
      * @return FebsResponse
@@ -78,6 +79,23 @@ public class GlobalExceptionHandler {
             message.append(pathArr[1]).append(violation.getMessage()).append(",");
         }
         message = new StringBuilder(message.substring(0, message.length() - 1));
+        return new FebsResponse().code(HttpStatus.BAD_REQUEST).message(message.toString());
+    }
+
+    /**
+     * 统一处理请求参数校验(json)
+     *
+     * @param e ConstraintViolationException
+     * @return FebsResponse
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public FebsResponse handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringBuilder message = new StringBuilder();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            message.append(error.getField()).append(error.getDefaultMessage()).append(",");
+        }
+        message = new StringBuilder(message.substring(0, message.length() - 1));
+        log.error(message.toString(), e);
         return new FebsResponse().code(HttpStatus.BAD_REQUEST).message(message.toString());
     }
 
