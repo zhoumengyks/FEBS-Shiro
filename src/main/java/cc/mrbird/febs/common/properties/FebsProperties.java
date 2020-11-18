a883bf188924aaabb6a262584d17f6f35eb2e0e5
MrBird <852252810@qq.com>
2020-11-18 17:03:29 +0800

    use spring.config.import

--- src/main/java/cc/mrbird/febs/common/properties/FebsProperties.java
+++ src/main/java/cc/mrbird/febs/common/properties/FebsProperties.java
@@ -3,14 +3,12 @@ package cc.mrbird.febs.common.properties;
 import lombok.Data;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author MrBird
  */
 @Data
 @SpringBootConfiguration
-@PropertySource(value = {"classpath:febs.properties"})
 @ConfigurationProperties(prefix = "febs")
 public class FebsProperties {
 
--- src/main/java/cc/mrbird/febs/common/properties/ShiroProperties.java
+++ src/main/java/cc/mrbird/febs/common/properties/ShiroProperties.java
@@ -8,11 +8,32 @@ import lombok.Data;
 @Data
 public class ShiroProperties {
 
+    /**
+     * session 超时时间，单位为秒
+     */
     private long sessionTimeout;
+    /**
+     * rememberMe cookie有效时长，单位为秒
+     */
     private int cookieTimeout;
+    /**
+     * 免认证的路径配置，如静态资源等
+     */
     private String anonUrl;
+    /**
+     * 登录 url
+     */
     private String loginUrl;
+    /**
+     * 首页 url
+     */
     private String successUrl;
+    /**
+     * 登出 url
+     */
     private String logoutUrl;
+    /**
+     * 未授权跳转 url
+     */
     private String unauthorizedUrl;
 }
